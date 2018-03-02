aa5b73a4b797c9e84e18c3481b95ab457c2cf2b4
profaris22 <aris.prof22@gmail.com>
2018-03-02 16:41:34 +0700

    Create plotADC.py

---
+++ plotADC.py
@@ -0,0 +1,44 @@
+import os
+import time
+from time import sleep 
+from datetime import datetime
+import Adafruit_ADS1x15
+import RPi.GPIO as GPIO
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+style.use('fivethirtyeight')
+fig=plt.figure()
+ax1=fig.add_subplot(1,1,1)
+adc=Adafruit_ADS1x15.ADS1115()
+GAIN=1
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN,pull_up_down = GPIO.PUD_UP)
+i=0
+open('adc.txt','w').close()
+
+while i<256:
+    i=i+1
+    file = open("adc.txt","a")
+    b=adc.read_adc(0,gain=GAIN)
+    x=(b/65536)*5
+    v="%.2f" % x
+    print(v)
+    file.write(str(i)+","+str(v)+"\n")
+    time.sleep(0.05)
+    
+def animate(i):
+    graph_data = open('adc.txt','r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(y)
+    ax1.clear()
+    ax1.plot(xs, ys)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
