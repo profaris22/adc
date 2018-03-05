ec11ae12c7323f7fb5630e22ee9d78a192c911b5
Aris Widodo,S.Si <aris.prof22@gmail.com>
2018-03-05 11:28:15 +0700

    Update plotADC.py

--- plotADC.py
+++ plotADC.py
@@ -16,32 +16,38 @@ GAIN=1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN,pull_up_down = GPIO.PUD_UP)
 i=0
-open('adc.txt','w').close()
 
-while i<256:
+while True:
     i=i+1
-    file = open("adc.txt","a")
-    b=adc.read_adc(0,gain=GAIN)
-    x=(b/65536)*5
-    v="%.2f" % x
-    print(v)
-    file.write(str(i)+","+str(v)+"\n")
-    time.sleep(0.05)
+    open('adc.txt','w').close()
+    if i < 10:
+        file = open("adc.txt","a")
+        b=adc.read_adc(0,gain=GAIN)
+        x=(b/65536)*5
+        v="%.2f" % x
+        print(v)
+        file.write(str(i)+","+str(v)+"\n")
+        time.sleep(0.05)
     
-def animate(i):
-    graph_data = open('adc.txt','r').read()
-    lines = graph_data.split('\n')
-    xs = []
-    ys = []
-    for line in lines:
-        if len(line) > 1:
-            x, y = line.split(',')
-            xs.append(x)
-            ys.append(y)
-    ax1.clear()
-    ax1.plot(xs, ys)
-ani = animation.FuncAnimation(fig, animate, interval=1000)
-plt.xlabel('Titik pengukuran')
-plt.ylabel("Tegangan (mv)")
-plt.title('Plotting Tegangan Forward model')
-plt.show()
+    def animate(i):
+         graph_data = open('adc.txt','r').read()
+         lines = graph_data.split('\n')
+         xs = []
+         ys = []
+         for line in lines:
+            if len(line) > 1:
+                x, y = line.split(',')
+                xs.append(x)
+                ys.append(y)
+            ax1.clear()
+            ax1.plot(xs, ys)
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    plt.xlabel('Titik pengukuran')
+    plt.ylabel("Tegangan (mv)")
+    plt.title('Plotting Tegangan Forward model')
+    plt.show()
+    
+    else:
+        i=0
+    
+  
